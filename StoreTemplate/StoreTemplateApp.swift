6d61a896acd405321563b35ab4a9015fa14d347b
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-01-21 12:34:13 +0100

    Initial Commit

---
+++ StoreTemplate/StoreTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreTemplateApp.swift
+//  StoreTemplate
+//
+//  Created by Nicola Kaleta on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct StoreTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
